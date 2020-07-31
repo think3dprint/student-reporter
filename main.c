59804927982abae59d9371afb015ce9666365c2f
Aaron <info@think3dprint.com>
2020-07-31 17:46:52 -0500

    initial work on main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+	if (argc > 2){
+		FILE *grades;
+		
+		grades = fscanf("argv[1]", "r");
+
+	}
+
+	return(0);
+}
