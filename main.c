060a3a1e50b263e07f08448b4228b8d0542724fd
Aaron <info@think3dprint.com>
2020-08-01 21:46:56 -0500

    more main.c work

--- main.c
+++ main.c
@@ -2,20 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+float average(float arrayGrades){
+	int arrayGrades, sum, loop;
+	sum = 0;
+	for(loop = 9; loop >= 0; loop --){
+		sum = sum + arrayGrades[loop];
+	}
+	float average = float sum / n;
+	return float average;
+}
+
+
+
 int main(int argc, char *argv[]){
 	if (argc > 2){
-		FILE *grades;
-		char *arrayGrades[1000];
+		char *arrayGrades[1001];
 		int numGrades = 0, n;
-		grades = fopen("argv[1]", "r");
+		FILE *grades = fopen("argv[1]", "r");
 		
 		if(grades == NULL){
 			printf("Error! could not open file.\n");
 			return(-1);
 		}
 		else{
-			printf("code would execute.\n");
-
+			//printf("code would execute.\n");
+			fgets(arrayGrades, 1001, grades);
+			grades = n;
+			fgets(arrayGrades, 1001, grades);
+			float 
 			}
 
 	}
